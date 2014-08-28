b396f08b5c184e1d55d5c543598b94b20bc0f120
presly808 <presly808@gmail.com>
2014-08-28 17:33:32 +0300

    sorter no duplicates task Skochinsiy

---
+++ src/test_task/Sorter.java
@@ -0,0 +1,81 @@
+package test_task;
+
+import java.util.ArrayList;
+import java.util.Arrays;
+
+/**
+ * Created by admin on 28.08.2014.
+ */
+public class Sorter {
+
+	private char[] sorted;
+	private char[] duplicates;
+	private int end;
+
+    //Here we use selection sort
+    public void sortWithNoDuplicates(char[] mas){
+    	sorted = mas;
+        duplicates = new char[mas.length];
+        int counterForDuplicates = 0;
+        end = mas.length - 1;
+
+
+        for(int i = 0; i < end; i++){
+            char value = mas[i]; //temp ceil for comparing(selection sort)
+            int minI = i; //temp index for comparing(selection sort)
+
+            for(int j = i + 1; j <= end; j++){
+                if(mas[j] < value){
+                    minI = j;
+                    value = mas[j];
+                } else if(mas[j] == value){
+                    duplicates[counterForDuplicates++] = mas[j]; // put into mas for duplicates
+                    swap(mas, j, end--);//duplicate go to end position, end limit will decrease
+                }
+            }
+
+            swap(mas, i, minI);
+        }
+        
+        trimToSize();
+
+    }
+    
+    private void trimToSize(){
+    	sorted = Arrays.copyOf(sorted, end + 1);
+    	duplicates = Arrays.copyOf(duplicates, duplicates.length - end - 1);
+    }
+    
+    
+
+    public char[] getSorted() {
+		return sorted;
+	}
+
+
+
+	public char[] getDuplicates() {
+		return duplicates;
+	}
+
+
+
+	private void swap(char[] mas, int a, int b){
+        char t = mas[a];
+        mas[a] = mas[b];
+        mas[b] = t;
+    }
+
+
+    public static void main(String[] args) {
+        Sorter sorter = new Sorter();
+    	char[] mas = {'4','1','r','N','2','g','5','1','9','g','5','4'};
+        sorter.sortWithNoDuplicates(mas);
+        System.out.println("SORTED MAS");
+        System.out.println(Arrays.toString(sorter.getSorted()));
+        System.out.println("DUPLICATES");
+        System.out.println(Arrays.toString(sorter.getDuplicates()));
+
+    }
+
+}
